6193a97d3b7c5f220b6a8070a6eb0c9ad666600c
satish486 <satya.chennaitcs@gmail.com>
2020-11-05 11:53:42 +0530

    done some thing

--- myscripts/helloworld.ps1
+++ myscripts/helloworld.ps1
@@ -32,7 +32,7 @@ echo "files are"+$files
 #foreach ($file in $files)
 #{
 echo "inside the for loop"
-    $relativepath = "test.html"
+    $relativepath = "test2.html"
 	echo $relativepath
     $uri = New-Object System.Uri("$url/$relativepath")
     echo "Uploading to " + $uri.AbsoluteUri
